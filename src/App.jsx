e1e961f5dad6a5ddc5dfa8605dd6b685d4371a0c
mmuthuvel126@gmail.com <Muthuvel-M>
2025-03-25 21:36:56 +0530

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Home } from './components/Home'
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+
 function Navbar() {
   return (
-    
-    <nav className=' flex justify-center bg-black text-white w-100% '>
-        
-        <ul className='flex m-6  '>
-            <li className='px-[50px] font-[900]'><a>Home</a></li>
-            <li className='px-[50px] font-[900]'><a>About</a></li>
-            <li className='px-[50px] font-[900]'><a>Project</a></li>
-            <li className='px-[50px] font-[900]'><a>Contact</a></li>
-        </ul>
-        
+    <nav className=' realative container mx-auto p-2'>
+      <div className='flex items-center justify-between'>
+        <div className="pt-2">
+          <img src='src\assets\icons\m-icon.png' class="w-12 h-fit" alt='logo'/>
+        </div>
+          <div className='md-flex space-x-10 '>
+            <a href='#' className='hover:text-orange-700'>Home</a>
+            <a href='#' className='hover:text-orange-700'>Home</a>
+            <a href='#' className='hover:text-orange-700'>Home</a>
+
+          </div>
+          <a href='#' className=''></a>
+        </div>
+
     </nav>
-  
   )
 }
 
